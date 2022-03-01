17b128841cda3691d4121237aa4b79999f54e727
Aydin Halimi <aydin_halimi1@baylor.edu>
2022-02-28 20:51:06 -0600

    Finish Ch.10HW

--- PatientBill.py
+++ PatientBill.py
@@ -10,12 +10,12 @@ def main():
 
     Pat = p.patient(Id,Name,Address,Phone,Veteran_Status)
 
-    print("ID:",Pat.get_id())
-    print("Name:",Pat.get_Name())
-    print("Address:",Pat.get_Address())
-    print("Phone:",Pat.get_Phone())
-    print("Veteran:",Pat.get_Vet())
-    print()
+    Pat.get__id()
+    Pat.get__Name()
+    Pat.get__Address()
+    Pat.get__Phone()
+    Pat.get__Vet()
+    
 
     Procedure = "Physical Exam"
     Date = "2/15/2022"
@@ -25,12 +25,12 @@ def main():
 
     Procedure1 = pr.Procedure(Procedure,Date,Practitioner,Charge,PatID)
 
-    print("Name:",Procedure1.get_Name())
-    print("Date:",Procedure1.get_Date())
-    print("Practitioner:",Procedure1.get_Practitioner())
-    print("Charge:",Procedure1.get_Charge())
-    print("Patient ID:",Procedure1.get_PatID())
-    print()
+    Procedure1.get_Name()
+    Procedure1.get_Date()
+    Procedure1.get_Practitioner()
+    Procedure1.get_Charges()
+    Procedure1.get_PatID()
+    
 
     Procedure = "MRI"
     Date = "2/15/2022"
@@ -40,12 +40,12 @@ def main():
 
     Procedure2 = pr.Procedure(Procedure,Date,Practitioner,Charge,PatID)
 
-    print("Name:",Procedure2.get_Name())
-    print("Date:",Procedure2.get_Date())
-    print("Practitioner:",Procedure2.get_Practitioner())
-    print("Charge:",Procedure2.get_Charge())
-    print("Patient ID:",Procedure2.get_PatID())
-    print()
+    Procedure2.get_Name()
+    Procedure2.get_Date()
+    Procedure2.get_Practitioner()
+    Procedure2.get_Charges()
+    Procedure2.get_PatID()
+
 
     Procedure = "CT Scan"
     Date = "2/17/2022"
@@ -55,12 +55,12 @@ def main():
 
     Procedure3 = pr.Procedure(Procedure,Date,Practitioner,Charge,PatID)
 
-    print("Name:",Procedure3.get_Name())
-    print("Date:",Procedure3.get_Date())
-    print("Practitioner:",Procedure3.get_Practitioner())
-    print("Charge:",Procedure3.get_Charge())
-    print("Patient ID:",Procedure3.get_PatID())
-    print()
+    Procedure3.get_Name()
+    Procedure3.get_Date()
+    Procedure3.get_Practitioner()
+    Procedure3.get_Charges()
+    Procedure3.get_PatID()
+    
 
     print("*** Patient Bill ***")
     print("Name:",Pat.get__Name())
@@ -69,32 +69,32 @@ def main():
     print()
 
     total_charge = 0
-    if Pat.get_id() == Procedure1.get_PatID():
+    if Pat.get__id() == Procedure1.get_PatID():
         print("Name:",Procedure1.get_Name())
         print("Date:",Procedure1.get_Date())
         print("Practitioner:",Procedure1.get_Practitioner())
-        print("Charge:",Procedure1.get_Charges())
+        print("Charge:","${:.2f}".format(Procedure1.get_Charges()))
         print()
 
         total_charge += Procedure1.get_Charges()
-    if Pat.get_id() == Procedure2.get_PatID():
+    if Pat.get__id() == Procedure2.get_PatID():
         print("Name:",Procedure2.get_Name())
         print("Date:",Procedure2.get_Date())
         print("Practitioner:",Procedure2.get_Practitioner())
-        print("Charge:",Procedure2.get_Charges())
+        print("Charge:","${:.2f}".format(Procedure2.get_Charges()))
         print()
 
         total_charge += Procedure2.get_Charges()
-    if Pat.get_id() == Procedure3.get_PatID():
+    if Pat.get__id() == Procedure3.get_PatID():
         print("Name:",Procedure3.get_Name())
         print("Date:",Procedure3.get_Date())
         print("Practitioner:",Procedure3.get_Practitioner())
-        print("Charge:",Procedure3.get_Charges())
+        print("Charge:","${:.2f}".format(Procedure3.get_Charges()))
         print()
 
         total_charge += Procedure3.get_Charges()
 
-    if Pat.get_veteran() == True:
+    if Pat.get__Vet() == True:
         total_charge *= .60
     else:
         total_charge
